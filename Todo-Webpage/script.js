f8e0964b2d4d572c9df896c19ba2bea1c8d55f56
VijayJsDev <vijayb2097@gmail.com>
2023-10-13 17:33:21 +0530

    Debug & Adding Bootstrap

--- Todo-Webpage/script.js
+++ Todo-Webpage/script.js
@@ -1,11 +1,16 @@
 const inputField = document.getElementById('inputField');
 const listContainer = document.getElementById('listContainer');
 
-const addTask = () => {
+function addTask() {  
+    if(inputField.value === ""){
+        alert("Input Field Should Not Be Empty");
+    }  else{
     let newTask = inputField.value;
-    let li = document.createElement('li')
+    let li = document.createElement('li');
     li.textContent = newTask;
     li.addEventListener("click", function () {
         this.remove();
     })
-};
+    listContainer.append(li)
+    inputField.value = "";
+}};
