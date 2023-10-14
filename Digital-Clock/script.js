4a6700e6416efb45ce5b468157f2875da1c00064
VijayJsDev <vijayb2097@gmail.com>
2023-10-14 17:02:21 +0530

    Initial Commit

---
+++ Digital-Clock/script.js
@@ -0,0 +1,15 @@
+function displayTime() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    
+    hours = (hours < 10 ? "0": "") + hours;
+    minutes = (minutes < 10 ? "0": "") + minutes;
+    seconds = (seconds < 10 ? "0": "") + seconds;
+
+    let timeString = hours + ":" +  minutes + ":" + seconds;
+    document.getElementById('time').innerHTML = timeString;
+}
+
+setInterval(displayTime, 1000);
