08bea49b0d4055fdcd390225562767c983a25751
VijayJsDev <vijayb2097@gmail.com>
2023-12-07 09:22:46 +0530

    Basic Structure And Functionality Added

--- Food-Ordering/script.js
+++ Food-Ordering/script.js
@@ -1,14 +1,46 @@
-let qty = 0;
-let newPrice = 0
-function incrementHandler(){
-   qty++;
-   document.getElementById('cartValue').innerHTML = qty;
-}
+document.addEventListener('DOMContentLoaded', function(){
+    const products = document.querySelectorAll('.product');
 
-function decrementHandler(){
-    if(qty>0){
-     qty--;
-     document.getElementById('cartValue').innerHTML = qty;
-    }
-}
+    products.forEach(product => {
+        const addToCart = product.querySelector('.addToCart');
+        const increaseButton = product.querySelector('.increase');
+        const decreaseButton = product.querySelector('.decrease');
+        const productName = product.querySelector('.name').innerText;
+        const productPrice = parseFloat(product.querySelector('.price').innerText);
+        let quantity = 0;
 
+        addToCart.addEventListener("click", function(){
+            const cartItems = document.getElementById('cartItems');
+            const cartItem = document.createElement('div');
+            const totalPrice = productPrice * quantity;
+            
+            if(quantity > 0){
+                cartItem.innerHTML = `
+                <ul style="display:flex; justify-content:space-around; text-decoration:none">
+                    <li>Product Name: ${productName}</li>
+                    <li>Product Price: $${productPrice}</li>
+                    <li>Quantity: ${quantity}</li>
+                    <li>Total Price: $${totalPrice}</li>
+                </ul>`;
+                cartItems.appendChild(cartItem);
+            }
+        });
+
+        increaseButton.addEventListener("click", function(){
+            quantity++;
+            updateQuantity();
+        });
+
+        decreaseButton.addEventListener("click", function(){
+            if(quantity > 0){
+                quantity--;
+            }
+            updateQuantity();
+        });
+
+        const updateQuantity = () => {
+            const qty = product.querySelector('.quantity');
+            qty.innerText = quantity;
+        };
+    });
+});
